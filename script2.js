7c93e23c73f2224ef735ec0ad0bf62403813bd40
Rogier van der Gaag <42837230+RMvanderGaag@users.noreply.github.com>
2020-04-08 12:23:36 +0200

    New script added

--- script2.js
+++ script2.js
@@ -1,19 +1,14 @@
 //globale variablen
 var statementNumber = 0;
-var a = 100 / subjects.length;
-const plusWith = 100 / subjects.length;
 const bigParty = 10;
 
 var position = ["pro", "none", "contra"];
-var data = [];
 var resultParties = [];
 
-var partyOpinion = document.getElementById("partyOpinion");
-var opinionContainer = document.getElementById("opinionContainer");
 var stellingTitle = document.getElementById("stellingTitle");
 var stellingDes = document.getElementById("stellingDes");
 
-
+//Voegt een nieuwe property aan het object toe
 subjects.forEach(subject => {
     subject.myOpinion = '';
     subject.weight = false;
@@ -24,31 +19,41 @@ parties.forEach(partie => {
 });
 
 function start(){
+    //Laad de pagina met de stellingen
     document.getElementById("homePage").style.display = "none";
     document.getElementById("stellingPage").style.display = "block";
 
+    //Zet de eerste vraag klaar
     stellingTitle.innerHTML = subjects[0].title;
     stellingDes.innerHTML = subjects[0].statement;
 }
 
+
 function getOpinion(opinion){
+    //De mening word toegevoegd aan de property
     subjects[statementNumber].myOpinion = opinion;
+    //Ook word de waarde van de checkbox geregistreerd in de property 
     subjects[statementNumber].weight = document.getElementById('important').checked
+    //Nieuwe functie word uitgevoerd
     nextStatement();
 }
 
+//Nieuwe stelling word geladen, wanneer je bij de laatste vraag is word er een functie aangeroepen.
 function nextStatement(){
     if(statementNumber < subjects.length - 1){
+        //Nieuwe stelling word geladen
         document.getElementById('important').checked = false;
         statementNumber++;
         stellingTitle.innerHTML = subjects[statementNumber].title;
         stellingDes.innerHTML = subjects[statementNumber].statement;
         checkStatement(subjects[statementNumber].myOpinion);
     }else{
+        //Na de laatste vraag word er een nieuwe functie aangeroepn
        matchOpinions();
     }
 }
 
+//Als de gebruiker op het pijltje terug klikt dan word de vorige vraag geladen, als ben je bij de laatste vraag word de home pagina weer getoond
 function previousStatement(){
     if(statementNumber !== 0){
         statementNumber--;
@@ -62,9 +67,12 @@ function previousStatement(){
 }
 
 function matchOpinions(){
+    //Er word door de subjects en de partijen geloopt
     for(var i = 0; i < subjects.length; i++){
-        for(var j = 0; j < subjects[i].parties.length; j++){
+        for(var j = 0; j < parties.length; j++){
+            //Daarna word er gekeen of de mening van de partijen overeenkomt met jou mening
             if(subjects[i].myOpinion == subjects[i].parties[j].position){
+                //Er word gekeken of de vraag belangrijk is en daarna worden de punten opgeteld 
                 if(subjects[i].weight == false){
                     parties[j].points += 1;
                 }else{
@@ -77,20 +85,24 @@ function matchOpinions(){
 }
 
 function showSelectPartiePage(){
+    //Nieuwe pagina word geladen
     document.getElementById("stellingPage").style.display = "none";
     document.getElementById("importantContainer").style.display = "block";
 
+    //De partijen worden op volgorde gezet met de meeste punten
     parties.sort(function(a,b){
         return b.points - a.points;
       });
     
-      for (var m = 0; m < parties.length; m++) {
+    //De partijen worden getoond 
+    for (var m = 0; m < parties.length; m++) {
         var p = document.createElement("p");
         p.innerHTML = parties[m].long + '<b> ' + parties[m].points + '</b>';
         document.getElementById("col1").appendChild(p);
       }
 }
 
+//Als de gebruiker terug gaat naar een eerder ingevulde vraag word het antwoord wat je daar in hebt gevuld getoond
 function checkStatement(opinion){
     document.getElementById('important').checked = false;
     for(var k = 0; k < position.length; k++){
@@ -106,4 +118,39 @@ function checkStatement(opinion){
     }else{
         document.getElementById(opinion).style.background = 'rgb(1, 180, 220)';
     }
+}
+
+//Deze functie word aangeroepen als de gebruiken de zittende partijen selecteerd
+function getCertainParties(){
+    resultParties = [];
+    resultParties = parties.filter(function(partie) {
+      return partie.secular == true;
+    });
+  }
+  
+//Deze functie word aangeroepen als de gebruiken alle partijen selecteerd
+function getAllParties(){
+    resultParties = parties;
+}
+  
+//Alleen de grote partijen worden geetoond
+function getBigParties(){
+    resultParties = [];
+    resultParties = parties.filter(function(partie) {
+      return partie.size >= bigParty;
+    })
+}
+
+//De Resultaat pagina word geladen
+function showResultPage(){
+    if(resultParties.length == 0){
+        resultParties = parties;
+      }
+    document.getElementById('importantContainer').style.display = 'none';
+    document.getElementById('resultContainer').style.display = 'block';
+
+    //De top 3 partijen worden laten zien
+    document.getElementById('1stPlace').innerHTML += resultParties[0].name;
+    document.getElementById('2ndPlace').innerHTML += resultParties[1].name;
+    document.getElementById('3thPlace').innerHTML += resultParties[2].name;
 }
